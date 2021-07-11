8879cb58a4295a138b2ccc0f2ff392a5f0baf93e
Sutariya21 <sutariya2001@gmail.com>
2021-07-11 14:01:42 +0530

    theme extrected

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_catelog/pages/home_page.dart';
 import 'package:flutter_catelog/pages/login_page.dart';
 import 'package:flutter_catelog/utils/routes.dart';
-import 'package:google_fonts/google_fonts.dart';
+import 'package:flutter_catelog/widget/themes.dart';
 
 void main() {
   runApp(MyApp());
@@ -13,12 +13,9 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       themeMode: ThemeMode.light,
-      theme: ThemeData(
-        primarySwatch: Colors.lightBlue,
-        fontFamily: GoogleFonts.lato().fontFamily),
-        darkTheme: ThemeData(
-        brightness: Brightness.dark,
-      ),
+      theme: MyTheme.lightTheme(context),
+        darkTheme: MyTheme.darkTheme(context),
+       
       debugShowCheckedModeBanner: false,
       initialRoute: MyRoutes.homeRoute,
       routes: {
