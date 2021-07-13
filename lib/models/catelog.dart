c06ce86d2a7d1fc7ca8f31329346ae356f418133
Sutariya21 <sutariya2001@gmail.com>
2021-07-13 10:25:38 +0530

    day12 complete

--- lib/models/catelog.dart
+++ lib/models/catelog.dart
@@ -1,5 +1,18 @@
+class CatelogModel {
+static final items = [Item(
+  id: 1,
+  name: "iphone 12 pro",
+  desc: "Apple iphone 12th  generation",
+  price: 12000,
+  color: "#33505a",
+  image: "https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/iphone-12-pro-family-hero?wid=940&hei=1112&fmt=jpeg&qlt=80&.v=16040216630000"
+)];
+}
+
+
+
 class Item {
-  final String id;
+  final int id;
   final String name;
   final String desc;
   final num price;
@@ -9,11 +22,4 @@ class Item {
   Item({required this.id, required this.name, required this.desc, required this.price, required this.color, required this.image});
 }
 
-final products = [Item(
-  id: "code1",
-  name: "iphone 12 pro",
-  desc: "Apple iphone 12th  generation",
-  price: 12000,
-  color: "#33505a",
-  image: "https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/iphone-12-pro-family-hero?wid=940&hei=1112&fmt=jpeg&qlt=80&.v=16040216630000"
-)];
+
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,18 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_catelog/models/catelog.dart';
 import 'package:flutter_catelog/widget/drawer.dart';
+import 'package:flutter_catelog/widget/item_widget.dart';
 
 class Homepage extends StatelessWidget {
   final String name = "first one";
   @override
   Widget build(BuildContext context) {
+        final dummylist = List.generate(50, (index) => CatelogModel.items[0]);
+
     return Scaffold(
       appBar: AppBar(
         title: Text("Catelog App"),
       ),
-      body: Center(
-        child: Container(
-          child: Text("Welcome to flutter by $name"),
-        ),
+      body: Padding(
+        padding: const EdgeInsets.all(10.0),
+        child: ListView.builder(
+          itemCount: dummylist.length,
+          itemBuilder: (context, index) {
+          return ItemWidget(
+            item: dummylist[index],
+            );
+        }),
       ),
       drawer: MyDrawer(),
     );
